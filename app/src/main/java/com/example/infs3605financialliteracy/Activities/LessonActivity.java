fba81a5af06bcf6f1e612ec586849e7a92630f29
YilunZhou98 <44451422+YilunZhou98@users.noreply.github.com>
2020-04-23 14:49:05 +1000

    added taxes section

--- app/src/main/java/com/example/infs3605financialliteracy/Activities/LessonActivity.java
+++ app/src/main/java/com/example/infs3605financialliteracy/Activities/LessonActivity.java
@@ -111,8 +111,96 @@ public class LessonActivity extends AppCompatActivity {
                 YouTubeActivity.YOUTUBE_VIDEO = "jY3_w3bwt3M";
                 break;
             case 2:
-                textView.setText("Testing 2");
-                YouTubeActivity.YOUTUBE_VIDEO = "jY3_w3bwt3M";
+                textView.setText("When you earn money from work or investments, you usually have to pay tax on your income.\n" +
+                        "\n" +
+                        "The amount you pay depends on:\n" +
+                        "\n" +
+                        "how much you earn, and\n" +
+                        "any deductions or offsets you can claim\n" +
+                        "Income tax is usually paid to the government as you earn throughout the year.\n" +
+                        "\n" +
+                        "If you're an employee, your employer will deduct tax from each pay and send it to the Australian Taxation Office (ATO) on your behalf.\n" +
+                        "\n" +
+                        "If you're self-employed, you need to set aside and pay the money yourself.\n" +
+                        "\n" +
+                        "At the end of each financial year, most people need to lodge a tax return with the ATO. You can do it yourself online through myTax or with the help of an accountant or tax agent.\n" +
+                        "\n" +
+                        "In Australia, income is taxed on a sliding scale.\n" +
+                        "\n" +
+                        "The table below shows income tax rates for Australian residents aged 18 and over.\n" +
+                        "\n" +
+                        "Tax rates 2018–19\n" +
+                        "\n" +
+                        "Taxable income: 0–$18,200; Tax on this income: Nil\n" +
+                        "Taxable income: $18,201–$37,000; Tax on this income: 19c for each $1 over $18,200\n" +
+                        "Taxable income: $37,001–$90,000; Tax on this income: $3,572 plus 32.5c for each $1 over $37,000\n" +
+                        "Taxable income: $90,001–$180,000; Tax on this income: $20,797 plus 37c for each $1 over $90,000\n" +
+                        "Taxable income: $180,001 and over; Tax on this income: $54,097 plus 45c for each $1 over $180,000\n" +
+                        "\n" +
+                        "This means if you earned $60,000 per year, your tax would be calculated like this:\n" +
+                        "\n" +
+                        "$18,200 x nil = $0\n" +
+                        "\n" +
+                        "$18,800 ($37,000–$18,200) x 19c = $3,572\n" +
+                        "\n" +
+                        "$23,000 ($60,000–$37,000) x 32.5c = $7,475\n" +
+                        "\n" +
+                        "$3,572 + $7,475 =  $11,047\n" +
+                        "\n" +
+                        "\n" +
+                        "These rates do not include the Medicare levy.\n" +
+                        "\n" +
+                        "If you're a foreign resident, a working holiday maker, or under age 18, see your individual income tax rates on the ATO website.\n" +
+                        "\n" +
+                        "Medicare levy and surcharge\n" +
+                        "Most people pay a Medicare levy, which is 2% of your taxable income. The levy is charged as part of your yearly income tax assessment.\n" +
+                        "\n" +
+                        "If you're on a low income, your levy may be reduced, or you may not have to pay it at all. If you're on a higher income, you may have also to pay a Medicare levy surcharge of between 1.0 and 1.5% of your taxable income. It depends on the level of private health insurance you have and how much you earn.\n" +
+                        "\n" +
+                        "See Medicare levy on the ATO website for details.\n" +
+                        "\n" +
+                        "Your taxable income is the income you must pay tax on, minus your tax deductions and offsets.\n" +
+                        "\n" +
+                        "Income that is taxable\n" +
+                        "Income that you must pay tax on includes money from: employment, pensions and annuities, most government payments, investments, capital gains, income from trusts, partnerships or businesses, and foreign income\n" +
+                        "\n" +
+                        "The ATO has more information on income you must declare in your tax return.\n" +
+                        "\n" +
+                        "Income that is not taxable\n" +
+                        "You will not have to pay tax on: lottery winnings and other prizes, small gifts or birthday presents, some government payments, child support, the tax-free portion of your redundancy payment, and government super co-contributions\n" +
+                        "The ATO has more details on amounts not included as income.\n" +
+                        "\n" +
+                        "Reducing the tax you pay\n" +
+                        "You may be able to reduce the amount of tax you pay if: you're entitled to tax deductions or offsets, or\n" +
+                        "you choose to salary package (salary sacrifice)\n" +
+                        "\n" +
+                        "Tax deductions\n" +
+                        "Tax deductions reduce your taxable income before the tax is calculated.\n" +
+                        "\n" +
+                        "Common tax deductions include: work-related expenses, self-education expenses, charitable donations, the cost of managing your tax affairs (for example, paying an accountant)\n" +
+                        "See deductions you can claim on the ATO website for details.\n" +
+                        "\n" +
+                        "Tax offsets\n" +
+                        "Tax offsets, also known as rebates, directly reduce the amount of tax payable. They are applied after the tax has been calculated.\n" +
+                        "\n" +
+                        "Common tax offsets include offsets for: low-income and middle-income earners, taxpayers with a invalid relative, pensioners and senior Australians, the taxable portion of a superannuation income stream\n" +
+                        "\n" +
+                        "See offsets and rebates on the ATO website for more information.\n" +
+                        "\n" +
+                        "Salary packaging\n" +
+                        "Salary packaging is when you 'package' your income into salary and benefits. For example, you may arrange to receive less salary in exchange for superannuation or car payments.\n" +
+                        "\n" +
+                        "If you reduce your salary in this way, you can reduce your taxable income. See salary packaging.\n" +
+                        "\n" +
+                        "Where to get help with tax\n" +
+                        "Individual income tax can be complex, and everyone's situation is different.\n" +
+                        "\n" +
+                        "If you want to get professional advice, think about choosing an accountant to help manage your tax and lodge your tax return.\n" +
+                        "\n" +
+                        "You can also ask general questions about tax through the ATO Community.\n" +
+                        "\n" +
+                        "See lodging your tax return on the ATO website for information about preparing your annual return.\n");
+                YouTubeActivity.YOUTUBE_VIDEO = "FeqwZwQcrj8";
                 break;
 
         }
